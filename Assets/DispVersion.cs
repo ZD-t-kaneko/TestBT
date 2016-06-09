88284c379cc180971dc72b6005fdba7ab7c90c8b
t-kaneko <t-kaneko@zerodiv.jp>
2016-06-09 16:53:54 +0900

    初回コミット

---
+++ Assets/DispVersion.cs
@@ -0,0 +1,52 @@
+﻿//------------------------------------------------------------------
+// 実行(ビルド)日時をOnGUI()を用いて表示する
+// 表示するシーンのオブジェクトにアタッチしておく
+//------------------------------------------------------------------
+using UnityEngine;
+using System.Reflection;
+using System;
+
+
+[assembly: AssemblyVersion("1.0.*")]
+public class DispVersion : MonoBehaviour
+{
+    public bool ShowVersionInformation = true;          // 表示フラグ
+
+    bool flgInit = false;
+    string strVersion;
+    Rect position = new Rect(0, 0, 200, 20);            // 表示矩形(x,yは別途産出している)
+    Color dispColor = Color.white;
+ 
+    // バージョン情報の産出
+    void Awake()
+    {
+        if (flgInit == true)
+            return;
+        flgInit = true;
+
+        // 表示位置の産出(画面左下に表示する)
+        position.x = 10f;
+        position.y = Screen.height - position.height - 10f;
+
+        // バージョン情報を文字列として取得しておく
+        Assembly asm = Assembly.GetExecutingAssembly();
+        System.Version ver = asm.GetName().Version;
+        DateTime buildDateTime = new DateTime(2000, 1, 1, 0, 0, 0);
+        buildDateTime = buildDateTime.AddDays(ver.Build);
+        buildDateTime = buildDateTime.AddSeconds(ver.Revision * 2);
+        strVersion = buildDateTime.ToString("yyyy/MM/dd HH:mm");
+    }
+
+    // バージョンを画面に表示する
+    void OnGUI()
+    { 
+        if (!ShowVersionInformation) { 
+            return; 
+        }
+
+        // バージョンを画面に表示する
+        GUI.contentColor = Color.white;
+        GUI.Label (position, string.Format("ver:{0}", strVersion)); 
+    }
+
+}
---
+++ Assets/TestKeyboard.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestKeyboard : MonoBehaviour {
+    string msg = "";
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 pos = transform.position;
+        Vector3 rot = transform.eulerAngles;
+
+        float moveVal = 0.1f;
+        float rotVal = 360.0f / (60.0f * 3.0f);
+
+        if (Input.GetKeyDown(KeyCode.Joystick1Button1) ||
+            Input.GetKeyDown(KeyCode.Space))
+        {
+            pos = Vector3.zero;
+            rot = Vector3.zero;
+        }
+
+        float mvY = Input.GetAxisRaw("Vertical");
+        float mvX = Input.GetAxisRaw("Horizontal");
+        if (mvX >= 0.5f)
+            rot.y += rotVal;
+        if (mvX <= -0.5f)
+            rot.y -= rotVal;
+        if (Input.GetKey(KeyCode.A))
+            rot.y += rotVal;
+        if (Input.GetKey(KeyCode.D))
+            rot.y -= rotVal;
+
+        if (mvY >= 0.5f)
+            pos.y += moveVal;
+        if (mvY <= -0.5f)
+            pos.y -= moveVal;
+        if (Input.GetKey(KeyCode.W))
+            pos.y += moveVal;
+        if (Input.GetKey(KeyCode.Z))
+            pos.y -= moveVal;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            pos.z += moveVal;
+        if (Input.GetKey(KeyCode.Joystick1Button3))
+            pos.z += moveVal;
+        if (Input.GetKey(KeyCode.DownArrow))
+            pos.z -= moveVal;
+        if (Input.GetKey(KeyCode.Joystick1Button0))
+            pos.z -= moveVal;
+
+
+        transform.eulerAngles = rot;
+        transform.position = pos;
+    }
+
+
+
+    void OnGUI()
+    {
+        //        msg = Input.GetAxisRaw("Vertical").ToString() + ", " + Input.GetAxisRaw("Horizontal").ToString();
+        //        GUI.Label(new Rect(0, 0, 200, 600), msg);
+    }
+}
